cd62db8d5406f597594612598497cd2339f18bab
priyanka babar <priyanka@qset.in>
2017-11-13 10:36:17 +0530

    added Rails-factory gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
-	
+  skip_before_action :authenticate_user!
+  
   def index
   end
 
   def dashboard
+  	redirect_to dashboard_home_index_path if current_user
   end
 
   def show
